1660565db9233ece50a88ed1f52c3725601e26c4
Matt Jones <me@mattjones.tech>
2024-06-08 15:12:23 -0400

    add TouchableOpacity

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Text, StyleSheet, View, Button } from "react-native";
+import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
 const HomeScreen = () => {
   return <View>
@@ -8,6 +8,9 @@ const HomeScreen = () => {
       title="Go To Components Demo"
       onPress={() => console.log('Button Pressed')}
     />
+    <TouchableOpacity onPress={() => console.log('List Pressed')}>
+      <Text>Go To List Demo</Text>
+    </TouchableOpacity>
   </View>
 };
 
