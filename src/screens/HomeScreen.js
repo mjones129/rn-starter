fc0914425842897782caf59e43fc47ba8e91371d
Matt Jones <me@mattjones.tech>
2024-06-08 15:05:39 -0400

    add basic button

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,14 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View, Button } from "react-native";
 
 const HomeScreen = () => {
-  return <Text style={styles.text}>Why... hello there.</Text>;
+  return <View>
+    <Text style={styles.text}>Why... hello there.</Text>
+    <Button
+      title="Go To Components Demo"
+      onPress={() => console.log('Button Pressed')}
+    />
+  </View>
 };
 
 const styles = StyleSheet.create({
