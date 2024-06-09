6630c1fd6c2f33b54d3f0f51dcf4fa74120a17fa
Matt Jones <me@mattjones.tech>
2024-06-09 09:53:37 -0400

    add random color generator function to generate colors at random

--- src/screens/ColorScreen.js
+++ src/screens/ColorScreen.js
@@ -4,7 +4,16 @@ import {View, Text, Button, StyleSheet} from 'react-native';
 const ColorScreen = () => {
   return <View>
     <Button title='Add Color' />
+    <View style={{height: 100, width: 100, backgroundColor: randomRGB()}}></View>
   </View>
+}
+
+const randomRGB = () => {
+  const red = Math.floor(Math.random() * 256);
+  const green = Math.floor(Math.random() * 256);
+  const blue = Math.floor(Math.random() * 256);
+
+  return `rgb(${red}, ${green}, ${blue})`;
 }
 
 const styles = StyleSheet.create({});
