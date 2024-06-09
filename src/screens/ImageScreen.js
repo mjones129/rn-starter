c279dd55f266d85d99381ac4db9d623f37580c56
Matt Jones <me@mattjones.tech>
2024-06-08 21:37:51 -0400

    add image score prop

--- src/screens/ImageScreen.js
+++ src/screens/ImageScreen.js
@@ -5,9 +5,9 @@ import ImageDetail from '../components/ImageDetail.js';
 const ImageScreen = () => {
   return <View>
     <Text style={styles.textSize}>Hello World From Image Screen</Text>
-    <ImageDetail title="Forest" image={require('../../assets/forest.jpg')}/>
-    <ImageDetail title="Beach" image={require('../../assets/beach.jpg')}/>
-    <ImageDetail title="Mountain" image={require('../../assets/mountain.jpg')}/>
+    <ImageDetail title="Forest" image={require('../../assets/forest.jpg')} score='7'/>
+    <ImageDetail title="Beach" image={require('../../assets/beach.jpg')} score='8'/>
+    <ImageDetail title="Mountain" image={require('../../assets/mountain.jpg')} score='9'/>
   </View>
 }
 
