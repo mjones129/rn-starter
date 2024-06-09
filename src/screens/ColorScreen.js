4ca74a1d4ee57f223c3ba509c0c5406747c7bcd5
Matt Jones <me@mattjones.tech>
2024-06-09 16:05:31 -0400

    log random colors to the console

--- src/screens/ColorScreen.js
+++ src/screens/ColorScreen.js
@@ -1,9 +1,16 @@
-import React from "react";
+import React, {useState} from "react";
 import {View, Text, Button, StyleSheet} from 'react-native';
 
 const ColorScreen = () => {
+  const [colors, setColors] = useState([]);
+  console.log(colors);
   return <View>
-    <Button title='Add Color' />
+    <Button
+      title='Add Color' 
+      onPress={() => {
+        setColors([...colors, randomRGB()]);
+      }}
+    />
     <View style={{height: 100, width: 100, backgroundColor: randomRGB()}}></View>
   </View>
 }
